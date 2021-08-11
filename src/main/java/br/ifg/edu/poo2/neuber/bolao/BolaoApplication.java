84824dc5def0df7ca820586e82178efc95efc6d3
xneuber <49963541+xneuber@users.noreply.github.com>
2021-08-11 18:43:23 -0300

    Add files via upload

---
+++ src/main/java/br/ifg/edu/poo2/neuber/bolao/BolaoApplication.java
@@ -0,0 +1,13 @@
+package br.ifg.edu.poo2.neuber.bolao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ifg/edu/poo2/neuber/bolao/BolaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifg.edu.poo2.neuber.bolao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
